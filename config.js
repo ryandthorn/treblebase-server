0654c1f7857329d0a6a463c81882f5dac42b950f
Ryan Thorn <ryandthorn@gmail.com>
2019-03-21 16:38:15 -0700

    Finish resolving conflict

--- config.js
+++ config.js
@@ -6,7 +6,4 @@ exports.PORT = process.env.PORT || 8080;
 exports.DATABASE_URL = process.env.DATABASE_URL;
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL;
 exports.JWT_SECRET = process.env.JWT_SECRET;
-<<<<<<< HEAD
-=======
-exports.JWT_EXPIRY = Math.floor(Date.now() / 1000) + (60 * 60);
->>>>>>> 151ccb9042172df1bf3e2e7d17d95a38ee184dc6
+exports.JWT_EXPIRY = Math.floor(Date.now() / 1000) + 60 * 60;

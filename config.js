7d56de0c106d24e7bf392d7a7a4c56aa31a6ef70
Ryan Thorn <ryandthorn@gmail.com>
2019-03-21 15:36:21 -0700

    Add base auth folder

--- config.js
+++ config.js
@@ -6,3 +6,4 @@ exports.PORT = process.env.PORT || 8080;
 exports.DATABASE_URL = process.env.DATABASE_URL;
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL;
 exports.JWT_SECRET = process.env.JWT_SECRET;
+exports.JWT_EXPIRY = Math.floor(Date.now() / 1000) + (60 * 60);

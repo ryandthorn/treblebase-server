4a65bd4ae6d8f5fd29900c0b70d2e1c999b4b626
Ryan Thorn <ryandthorn@gmail.com>
2019-03-20 17:08:01 -0700

    Setup dotenv, config

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 "use strict";
-
+const dotenv = require("dotenv");
+dotenv.config();
 exports.CLIENT_ORIGIN = process.env.DATABASE_URL || "http://localhost";
 exports.PORT = process.env.PORT || 8080;
+exports.DATABASE_URL = process.env.DATABASE_URL;
+exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL;
+exports.JWT_SECRET = process.env.JWT_SECRET;

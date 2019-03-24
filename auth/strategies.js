2b2c4bcf7297feafa85686e91a174f49d6b7e80a
Ryan Thorn <ryandthorn@gmail.com>
2019-03-23 19:51:19 -0700

    Pass option to use 'email' instead of username

--- auth/strategies.js
+++ auth/strategies.js
@@ -5,35 +5,38 @@ const { Strategy: JwtStrategy, ExtractJwt } = require("passport-jwt");
 const { Artist } = require("../users/models");
 const { JWT_SECRET } = require("../config");
 
-const localStrategy = new LocalStrategy((email, password, callback) => {
-  let user;
-  Artist.findOne({ email: email })
-    .then(_user => {
-      user = _user;
-      if (!user) {
-        return Promise.reject({
-          reason: "LoginError",
-          message: "Incorrect email or password"
-        });
-      }
-      return user.validatePassword(password);
-    })
-    .then(isValid => {
-      if (!isValid) {
-        return Promise.reject({
-          reason: "LoginError",
-          message: "Incorrect email or password"
-        });
-      }
-      return callback(null, user);
-    })
-    .catch(err => {
-      if (err.reason === "LoginError") {
-        return callback(null, false, err);
-      }
-      return callback(err, false);
-    });
-});
+const localStrategy = new LocalStrategy(
+  { usernameField: "email", session: false },
+  (email, password, callback) => {
+    let user;
+    Artist.findOne({ email })
+      .then(_user => {
+        user = _user;
+        if (!user) {
+          return Promise.reject({
+            reason: "LoginError",
+            message: "Incorrect email or password"
+          });
+        }
+        return user.validatePassword(password);
+      })
+      .then(isValid => {
+        if (!isValid) {
+          return Promise.reject({
+            reason: "LoginError",
+            message: "Incorrect email or password"
+          });
+        }
+        return callback(null, user);
+      })
+      .catch(err => {
+        if (err.reason === "LoginError") {
+          return callback(null, false, err);
+        }
+        return callback(err, false);
+      });
+  }
+);
 
 const jwtStrategy = new JwtStrategy(
   {

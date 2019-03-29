d9c41743be4111040428cd973f73562043fbffa0
Ryan Thorn <ryandthorn@gmail.com>
2019-03-29 10:05:20 -0700

    Reorganize /users tests to highlight CRUD

--- test/test-users.js
+++ test/test-users.js
@@ -30,75 +30,76 @@ describe("/api/users", function () {
     return Artist.deleteOne({ email: "example@email.com" });
   });
 
-  it("should retrieve serialized user info by ID in JWT on GET", function () {
-    // login
-    // check JWT data
-    // use ID to GET user info
-    const testUser = {
-      firstName: "Test",
-      lastName: "User",
-      email: "test@email.com",
-      password: "thinkful999"
-    }
+  describe("CREATE", function () {
+    it("should create a new user on POST", function () {
+      return chai
+        .request(app)
+        .post("/api/users")
+        .send(user)
+        .then(res => {
+          expect(res).to.have.status(201);
+          expect(res).to.be.an("object");
+          expect(res.body).to.have.keys("email", "id");
+          expect(res.body.email).to.equal(email);
+          return Artist.findOne({ email });
+        })
+        .then(user => {
+          expect(user).to.not.be.null;
+          expect(user.email).to.equal(email);
+          return user.validatePassword(password);
+        })
+        .then(passTrue => {
+          expect(passTrue).to.be.true;
+        });
+    });
 
-    return chai
-      .request(app)
-      .post('/api/auth/login')
-      .send({ email: testUser.email, password: testUser.password })
-      .then(res => {
-        expect(res).to.have.status(200);
-        expect(res.body).to.have.key("authToken");
-        return res.body.authToken;
-      })
-      .then(JWT => {
-        return chai
-          .request(app)
-          .get('/api/users')
-          .set({ Authorization: `Bearer ${JWT}` })
-          .then(res => {
-            expect(res).to.have.status(200);
-            expect(res.body).to.contain.keys("email", "firstName", "lastName");
-            expect(res.body.email).to.equal("test@email.com");
-          });
-      });
+    it("should return a validation error on invalid POST request", function () {
+      return chai
+        .request(app)
+        .post("/api/users")
+        .send({ email, password, firstName }) // Missing lastName
+        .then(res => {
+          expect(res).to.have.status(422);
+          expect(res.body.reason).to.equal("ValidationError");
+          expect(res.body.message).to.equal("Missing field");
+          expect(res.body.location).to.equal("lastName");
+        });
+    });
   });
 
-  it("should create a new user on POST", function () {
-    return chai
-      .request(app)
-      .post("/api/users")
-      .send(user)
-      .then(res => {
-        expect(res).to.have.status(201);
-        expect(res).to.be.an("object");
-        expect(res.body).to.have.keys("email", "id");
-        expect(res.body.email).to.equal(email);
-        return Artist.findOne({ email });
-      })
-      .then(user => {
-        expect(user).to.not.be.null;
-        expect(user.email).to.equal(email);
-        return user.validatePassword(password);
-      })
-      .then(passTrue => {
-        expect(passTrue).to.be.true;
-      });
+  describe("READ", function () {
+    it("should retrieve serialized user info by ID in JWT on GET", function () {
+      const testUser = {
+        firstName: "Test",
+        lastName: "User",
+        email: "test@email.com",
+        password: "thinkful999"
+      }
+
+      return chai
+        .request(app)
+        .post('/api/auth/login')
+        .send({ email: testUser.email, password: testUser.password })
+        .then(res => {
+          expect(res).to.have.status(200);
+          expect(res.body).to.have.key("authToken");
+          return res.body.authToken;
+        })
+        .then(JWT => {
+          return chai
+            .request(app)
+            .get('/api/users')
+            .set({ Authorization: `Bearer ${JWT}` })
+            .then(res => {
+              expect(res).to.have.status(200);
+              expect(res.body).to.contain.keys("email", "firstName", "lastName");
+              expect(res.body.email).to.equal("test@email.com");
+            });
+        });
+    });
   });
 
-  it("should return a validation error on invalid POST request", function () {
-    return chai
-      .request(app)
-      .post("/api/users")
-      .send({ email, password, firstName }) // Missing lastName
-      .then(res => {
-        expect(res).to.have.status(422);
-        expect(res.body.reason).to.equal("ValidationError");
-        expect(res.body.message).to.equal("Missing field");
-        expect(res.body.location).to.equal("lastName");
-      });
-  });
-
-  describe("PUT", function () {
+  describe("UPDATE", function () {
     let jwt;
 
     beforeEach(function () {
@@ -171,31 +172,33 @@ describe("/api/users", function () {
           expect(res).to.have.status(400);
           expect(res.body.message).to.equal("Error: cannot update field 'invalid'");
         });
-    })
+    });
   });
 
-  it("should delete a user by ID in JWT on DELETE", function () {
-    return chai
-      .request(app)
-      .post("/api/users")
-      .send(user)
-      .then(res => {
-        expect(res).to.have.status(201)
-        return chai
-          .request(app)
-          .post("/api/auth/login")
-          .send({ email, password })
-          .then(res => {
-            expect(res).to.have.status(200);
-            expect(res.body).to.have.key("authToken");
-            return chai
-              .request(app)
-              .delete("/api/users")
-              .set({ Authorization: `Bearer ${res.body.authToken}` })
-              .then(res => {
-                expect(res).to.have.status(204);
-              })
-          })
-      })
+  describe("DELETE", function () {
+    it("should delete a user by ID in JWT on DELETE", function () {
+      return chai
+        .request(app)
+        .post("/api/users")
+        .send(user)
+        .then(res => {
+          expect(res).to.have.status(201)
+          return chai
+            .request(app)
+            .post("/api/auth/login")
+            .send({ email, password })
+            .then(res => {
+              expect(res).to.have.status(200);
+              expect(res.body).to.have.key("authToken");
+              return chai
+                .request(app)
+                .delete("/api/users")
+                .set({ Authorization: `Bearer ${res.body.authToken}` })
+                .then(res => {
+                  expect(res).to.have.status(204);
+                });
+            });
+        });
+    });
   });
 });
